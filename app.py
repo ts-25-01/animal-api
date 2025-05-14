c7a748ad24e12ac7d4c99ce1a257dce3d7580c86
AlexSch95 <95.schulz@googlemail.com>
2025-05-14 14:31:37 +0200

    added functionality to check if the animal is already in the list when using the post route - animal name gets converted to lower case on post and delete routes

--- app.py
+++ app.py
@@ -23,14 +23,17 @@ def add_animal():
     ## Funktion um die Daten im JSON-Format aus dem Request-Objekt zu bekommen
     data = request.get_json()
     ## in new_animal ist dann der Value vom Key "name" enthalten
-    new_animal = data.get("name")
-
-    animals.append(new_animal)
-    return f"{new_animal} wurde erfolgreich hinzugefügt", 201
+    new_animal = data.get("name").lower() #da unsere daten im localstorage einheitlich alle mit Kleinbuchstaben sind
+    if new_animal not in animals:   #Prüfen ob das Tier schon vorhanden ist, wenn nicht, dann wirds hinzugefügt (schneller als alles auf ein Set umzubauen
+        animals.append(new_animal)
+        return f"{new_animal} wurde erfolgreich hinzugefügt", 201
+    else:
+        return f"{new_animal} wurde nicht hinzugefügt, da es bereits in der Liste vorhanden ist", 404
 
 ## DELETE-Route, um ein Tier aus der Liste zu löschen
 @app.route("/api/animals/<name>", methods=['DELETE'])
 def delete_animal(name):
+    name = name.lower() #damit das was in der Route genannt wird im Lowercase geprüft wird, dass falls der User Dog eingibt, trotzdem dog gelöscht wird
     if name in animals:
         animals.remove(name)
         return f"{name} wurde aus der Liste gelöscht", 200
