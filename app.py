532e4b7e954cf824e70db7a3ff7583a1e3b70d75
Helen Haveloh <h.havelohh@gmail.com>
2025-06-02 12:10:50 +0200

    added statistis route

--- app.py
+++ app.py
@@ -617,17 +617,45 @@ def patch_owner(owner_id):
     con.close()
     return jsonify({"message": "Besitzer wurde geupdated"}), 200
 
+# Statistik-Route, um Daten aus beiden Tabellen anzuzeigen
+@app.route("/api/stats", methods=["GET"])
+def get_statistics():
+    """
+    Statistiken über Tiere und Besitzer
+    ---
+    responses:
+        200:
+            description: Statistiken über Tiere und Besitzer
+            examples:
+                application/json:
+                    - total_animals: 5
+                    - total_owners: 2
+    """
+    con = get_db_connection()
+    cur = con.cursor()
+    animal_count = cur.execute('SELECT COUNT(*) FROM Animals').fetchone()[0]
+    owner_count = cur.execute('SELECT COUNT(*) FROM Owners').fetchone()[0]
+    # Nach Gattung filtern
+    genus_stats = cur.execute(
+        '''
+        SELECT genus, COUNT(*) as count FROM Animals
+        WHERE genus IS NOT NULL
+        GROUP BY genus
+        '''
+    ).fetchall() # ("mammals", 3), ("birds", 2), ...
 
+    # genus_stats_rows = []
+    # for row in genus_stats:
+    #     genus_stats_rows.append(row["genus"]: row[])
 
-    # update_data = request.get_json()
-    # for animal in animals:
-    #     if animal["name"] == name:
-    #         animal.update(update_data)
-    #         # return f"{name} wurde geupdated", 200
-    #         return jsonify({"message": "Tier wurde geupdated"}), 200
-    # # return f"{name} wurde nicht gefunden", 404
-    # return jsonify({"message": "Tier wurde nicht gefunden"}), 404
+    con.close()
+    stats = {
+        "total_animals": animal_count,
+        "total_owners": owner_count,
+        "animals_by_genus": {row["genus"]: row["count"] for row in genus_stats}
+    }
 
+    return jsonify(stats), 200
 # App starten
 if __name__ == "__main__":
     init_db()
